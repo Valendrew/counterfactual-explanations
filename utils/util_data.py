4feeade12b442f11466b59b74697a527841b6543
Valendrew <andrea.valente99@gmail.com>
2023-03-18 18:33:57 +0100

    feat: download configuration and new plots

--- utils/util_data.py
+++ utils/util_data.py
@@ -1,9 +1,11 @@
+import math
 import typing as t
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelBinarizer, StandardScaler
 
 
 def read_csv(filename: str) -> pd.DataFrame:
@@ -45,3 +47,53 @@ def plot_dataframe(data, labels=None, vmin=-9, vmax=0.15, figsize=None, s=4):
             color=plt.get_cmap("tab10")(np.mod(labels, 10)),
         )
     plt.tight_layout()
+
+
+def encode_normalize_df(df, std_scaler=None, label_bin=None):
+    X_raw, y_raw = df.iloc[:, 1:], df.iloc[:, 0]
+
+    # standardization with mean 0 and unit variance
+    if std_scaler is None:
+        std_scaler = StandardScaler()
+        X = std_scaler.fit_transform(X_raw)
+    else:
+        X = std_scaler.transform(X_raw)
+
+    # encoding of the label
+    if label_bin is None:
+        label_bin = LabelBinarizer(neg_label=0, pos_label=1)
+        y = label_bin.fit_transform(y_raw)
+    else:
+        y = label_bin.transform(y_raw)
+
+    return X, y, std_scaler, label_bin
+
+
+def plot_correlated_features(feat_1, feat_2, n_cols=3, sx=4, sy=5):
+    # Plot w.r.t feat_1
+    n_rows = math.ceil(feat_1.nunique() / n_cols)
+    fig, axs = plt.subplots(
+        nrows=n_rows, ncols=n_cols, figsize=(sx * n_cols, sy * n_rows)
+    )
+    faxs = axs.ravel()
+
+    for i, v in enumerate(feat_1.drop_duplicates().sort_values()):
+        idx = feat_1[feat_1 == v].index
+        feat_2_sub = feat_2[idx].sort_values()
+        faxs[i].plot(range(len(idx)), feat_2_sub)
+
+        faxs[i].set_title(f"{feat_1.name}=={v}")
+        faxs[i].title.set_fontsize(9)
+
+        y_min, y_max, y_step = feat_2_sub.min(), feat_2_sub.max() + 1, 6
+        faxs[i].set_yticks(
+            np.arange(y_min, y_max, step=math.ceil((y_max - y_min) / y_step))
+        )
+        b_min, b_max, step = 0, len(idx), 6
+        faxs[i].set_xticks(
+            np.arange(b_min, b_max, step=math.ceil((b_max - b_min) / step))
+        )
+
+    for i in range(1, 1 + (n_rows * n_cols - feat_1.nunique())):
+        faxs[-i].set_visible(False)
+    fig.show()
